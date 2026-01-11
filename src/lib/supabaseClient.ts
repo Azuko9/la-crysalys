be4c5fc81aab54b2046d77a9384dbe3206daaf88
Azuko9 <azuko909@gmail.com>
2026-01-11 16:41:51 +0100

    feat: Update Supabase client import to use createBrowserClient for SSR support

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-// C'est cet objet "supabase" qu'on utilisera pour faire nos requêtes
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
